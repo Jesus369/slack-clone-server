59f870495eb58e41d2e65c95cae59c0c1ac42269
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-10 13:09:40 -0600

    Imported formatErrors file

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,17 +1,5 @@
-import bcrypt from "bcrypt";
-import _ from "lodash";
+import formatErrors from "../formatErrors";
 import { tryLogin } from "../auth";
-
-const formatErrors = (e, models) => {
-  /*If error is an instance of ValidationError*/
-  if (e instanceof models.sequelize.ValidationError) {
-    /*Map through errors and display this message*/
-    /*Using a lodash function to pick one instead of rendering the whole object*/
-    return e.errors.map(selected => _.pick(selected, ["path", "message"]));
-  }
-  /*Otherwise if it's not a defined error as in models.user, then display this message */
-  return [{ path: "name", message: "Something went wrong" }];
-};
 
 export default {
   Query: {
