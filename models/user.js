d4ffe4638d8e2f85be244dc199aecbb64484f9b0
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-10 11:18:40 -0600

    Inserted validation for hashed password

--- models/user.js
+++ models/user.js
@@ -1,37 +1,43 @@
-import bcrypt from 'brcypt'
+import bcrypt from "bcrypt";
 
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isAlphanumeric: {
-          args: true,
-          msg: "The username can only contain letters and numbers"
-        },
-        len: {
-          args: [3, 25],
-          msg: "The username must be between 3 - 25 characters long"
+  const User = sequelize.define(
+    "user",
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isAlphanumeric: {
+            args: true,
+            msg: "The username can only contain letters and numbers"
+          },
+          len: {
+            args: [3, 25],
+            msg: "The username must be between 3 - 25 characters long"
+          }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Invalid email"
+          }
+        }
+      },
+      password: DataTypes.STRING
+    },
+    {
+      hooks: {
+        afterValidate: async user => {
+          user.password = await bcrypy.hash(user.password, 12);
         }
       }
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isEmail: {
-          args: true,
-          msg: "Invalid email"
-        }
-      }
-    },
-    password: DataTypes.STRING
-  }, hooks: {
-    afterValidate: async (user) => {
-      user.password = await bcrypy.hash(password, 12)
     }
-  });
+  );
 
   User.associate = models => {
     User.belongsToMany(models.Team, {
