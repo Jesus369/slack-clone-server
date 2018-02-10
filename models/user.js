438b417e3fd62d72433d3c1754a22044ba7927a9
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-10 11:15:04 -0600

    Included validation for bcrypted password

--- models/user.js
+++ models/user.js
@@ -1,31 +1,53 @@
-export default(sequelize, DataTypes) => {
+import bcrypt from 'brcypt'
+
+export default (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
     username: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: "The username can only contain letters and numbers"
+        },
+        len: {
+          args: [3, 25],
+          msg: "The username must be between 3 - 25 characters long"
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Invalid email"
+        }
+      }
     },
     password: DataTypes.STRING
-  })
+  }, hooks: {
+    afterValidate: async (user) => {
+      user.password = await bcrypy.hash(password, 12)
+    }
+  });
 
-  User.associate = (models) => {
+  User.associate = models => {
     User.belongsToMany(models.Team, {
-      through: 'member',
+      through: "member",
       foreignKey: {
-        name: 'userId',
-        field: 'user_id',
-      },
-    })
-    User.belongsToMany(models.Channel, {
-      through: 'channel_member',
-      foreignKey: {
-        name: 'userId',
-        field: 'user_id',
+        name: "userId",
+        field: "user_id"
       }
-    })
-  }
-  return User
-}
+    });
+    User.belongsToMany(models.Channel, {
+      through: "channel_member",
+      foreignKey: {
+        name: "userId",
+        field: "user_id"
+      }
+    });
+  };
+  return User;
+};
