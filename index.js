acbf83a88990f120ff143b85a86eea34122d35dc
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-12 09:02:17 -0600

    Using Apollo Header's in our server

--- index.js
+++ index.js
@@ -1,54 +1,79 @@
-import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas";
+import express from "express";
+import bodyParser from "body-parser";
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import { makeExecutableSchema } from "graphql-tools";
-import bodyParser from "body-parser";
-import express from "express";
-import models from "./models";
 import path from "path";
+import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 
-const SECRET = "klfskfksgkjsh";
-const SECRET2 = "klfskfksgkjshljfsdhfshfkds";
+import models from "./models";
+import { refreshTokens } from "./auth";
 
-/*fileLoader will grab all schema files within the schema/resolvers folder*/
-/*mergeTypes/Resolvers will converted them into an array*/
-/*Newly added files will be added into the array*/
+const SECRET = "asiodfhoi1hoi23jnl1kejd";
+const SECRET2 = "asiodfhoi1hoi23jnl1kejasdjlkfasdd";
+
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, "./schema")));
+
 const resolvers = mergeResolvers(
   fileLoader(path.join(__dirname, "./resolvers"))
 );
 
-/*makeExecutable merges both Schemas together*/
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 });
 
-const graphqlEndpoint = "/graphql";
 const app = express();
 
 app.use(cors("*"));
 
-/*Passing in Schema folder.*/
-/*Passing in Models folder to create from resolvers folder*/
+const addUser = async (req, res, next) => {
+  const token = req.headers["x-token"];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers["x-refresh-token"];
+      const newTokens = await refreshTokens(
+        token,
+        refreshToken,
+        models,
+        SECRET,
+        SECRET2
+      );
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set("Access-Control-Expose-Headers", "x-token, x-refresh-token");
+        res.set("x-token", newTokens.token);
+        res.set("x-refresh-token", newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
+const graphqlEndpoint = "/graphql";
+
 app.use(
   graphqlEndpoint,
   bodyParser.json(),
-  graphqlExpress({
+  graphqlExpress(req => ({
     schema,
     context: {
       models,
-      user: { id: 1 },
+      user: req.user,
       SECRET,
       SECRET2
     }
-  })
+  }))
 );
 
-/*Telling graphiql what our graph endpoint is*/
 app.use("/graphiql", graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-/*sync will create all tables within Postgress assuming they don't already exist*/
 models.sequelize.sync({}).then(() => {
   app.listen(8081);
 });
