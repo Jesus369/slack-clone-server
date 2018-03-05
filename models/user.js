9bc2dc0f6ceabd05ea58f5543098dc73e8ab3185
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-04 21:58:48 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
-import bcrypt from "bcrypt";
+
+import bcrypt from 'bcrypt';
 
 export default (sequelize, DataTypes) => {
   const User = sequelize.define(
-    "user",
+    'user',
     {
       username: {
         type: DataTypes.STRING,
@@ -10,13 +11,13 @@ export default (sequelize, DataTypes) => {
         validate: {
           isAlphanumeric: {
             args: true,
-            msg: "The username can only contain letters and numbers"
+            msg: 'The username can only contain letters and numbers',
           },
           len: {
             args: [3, 25],
-            msg: "The username must be between 3 - 25 characters long"
-          }
-        }
+            msg: 'The username needs to be between 3 and 25 characters long',
+          },
+        },
       },
       email: {
         type: DataTypes.STRING,
@@ -24,45 +25,48 @@ export default (sequelize, DataTypes) => {
         validate: {
           isEmail: {
             args: true,
-            msg: "Invalid email"
-          }
-        }
+            msg: 'Invalid email',
+          },
+        },
       },
       password: {
         type: DataTypes.STRING,
         validate: {
           len: {
             args: [5, 100],
-            msg: "The password must be between 5 - 100 characters long"
-          }
-        }
-      }
+            msg: 'The password needs to be between 5 and 100 characters long',
+          },
+        },
+      },
     },
     {
       hooks: {
-        afterValidate: async user => {
+        afterValidate: async (user) => {
           const hashedPassword = await bcrypt.hash(user.password, 12);
+          // eslint-disable-next-line no-param-reassign
           user.password = hashedPassword;
-        }
-      }
-    }
+        },
+      },
+    },
   );
 
-  User.associate = models => {
+  User.associate = (models) => {
     User.belongsToMany(models.Team, {
-      through: "member",
+      through: models.Member,
       foreignKey: {
-        name: "userId",
-        field: "user_id"
-      }
+        name: 'userId',
+        field: 'user_id',
+      },
     });
+    // N:M
     User.belongsToMany(models.Channel, {
-      through: "channel_member",
+      through: 'channel_member',
       foreignKey: {
-        name: "userId",
-        field: "user_id"
-      }
+        name: 'userId',
+        field: 'user_id',
+      },
     });
   };
+
   return User;
 };
