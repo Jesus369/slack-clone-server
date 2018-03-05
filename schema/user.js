94f56d2f87b37fbf90202b7ed90d911ab1abb09c
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-04 22:03:26 -0600

    Mutation for logging in the user

--- schema/user.js
+++ schema/user.js
@@ -2,28 +2,26 @@ export default `
   type User {
     id: Int!
     username: String!
-    email: String!,
-    password: String!,
-    teams : [Team!]!
+    email: String!
+    teams: [Team!]!
   }
-
   type Query {
     getUser(id: Int!): User!
     allUsers: [User!]!
   }
-
   type RegisterResponse {
     ok: Boolean!
     user: User
     errors: [Error!]
   }
-
+  type LoginResponse {
+    ok: Boolean!
+    token: String
+    refreshToken: String
+    errors: [Error!]
+  }
   type Mutation {
-    register(username: String!, email: String!, password:String!): RegisterResponse!
+    register(username: String!, email: String!, password: String!): RegisterResponse!
+    login(email: String!, password: String!): LoginResponse!
   }
 `;
-
-/*type User : define what a User requires*/
-/*Query function : getUser passing in an id and will return "User"*/
-/*Query function : getUsers passing in an id and will return an array of "User"*/
-/*Mutation : Creating a user, passing in the fields required to create a user and returning "User"*/
