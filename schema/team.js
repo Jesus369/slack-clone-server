38569bd363a2f747c90422d40b1d611e9d771174
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-03-04 22:02:52 -0600

    Adding a team member to a team mutation

--- schema/team.js
+++ schema/team.js
@@ -1,16 +1,26 @@
 export default `
   type Team {
+    id: Int!
+    name: String!
     owner: User!
     members: [User!]!
     channels: [Channel!]!
   }
-
   type CreateTeamResponse {
+    ok: Boolean!
+    team: Team
+    errors: [Error!]
+  }
+  type Query {
+    allTeams: [Team!]!
+    inviteTeams: [Team!]!
+  }
+  type VoidResponse {
     ok: Boolean!
     errors: [Error!]
   }
-
   type Mutation {
     createTeam(name: String!): CreateTeamResponse!
+    addTeamMember(email: String!, teamId: Int!): VoidResponse!
   }
 `;
